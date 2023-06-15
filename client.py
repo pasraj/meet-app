9dca95caec1ff12b77c21725e26be91757a9eaa9
Raj Kumar ( Backend ) <rajkumar3205@gmail.com>
2023-06-15 21:58:27 +0530

    -

---
+++ client.py
@@ -0,0 +1,37 @@
+import cv2
+import socket
+import pickle
+import struct
+
+# Create socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_ip = '127.0.1.1'  # Replace with the actual IP address of the server
+port = 1234
+client_socket.connect((host_ip, port))
+data = b""
+payload_size = struct.calcsize("Q")
+
+# Loop to receive and display video frames
+while True:
+    while len(data) < payload_size:
+        packet = client_socket.recv(4*1024)  # 4K buffer size
+        if not packet:
+            break
+        data += packet
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q", packed_msg_size)[0]
+
+    while len(data) < msg_size:
+        data += client_socket.recv(4*1024)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    frame = pickle.loads(frame_data)
+    cv2.imshow("Client - Receiving", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+
+# Cleanup
+cv2.destroyAllWindows()
+client_socket.close()
---
+++ main.py
@@ -0,0 +1,41 @@
+import cv2
+import socket
+import pickle
+import struct
+
+# Create socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
+print('Host IP:', host_ip)
+port = 1234
+socket_address = (host_ip, port)
+
+# Bind socket to address and start listening
+server_socket.bind(socket_address)
+server_socket.listen(5)
+print("Listening for incoming connections...")
+
+# Accept a client connection
+client_socket, addr = server_socket.accept()
+print('Connected to:', addr)
+
+# Open video capture
+cap = cv2.VideoCapture(0)
+
+# Loop to send video frames
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (320, 240))
+    serialized_frame = pickle.dumps(frame)
+    message = struct.pack("Q", len(serialized_frame)) + serialized_frame
+    client_socket.sendall(message)
+    cv2.imshow('Server - Sending', frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+
+# Cleanup
+cap.release()
+cv2.destroyAllWindows()
+server_socket.close()
